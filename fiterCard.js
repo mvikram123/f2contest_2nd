fadbebb16828ac3b7406c17283dbd188020d8925
manbodhvikram <manbodhvikram@gmail.com>
2023-05-20 16:27:25 +0530

    f2contest

---
+++ fiterCard.js
@@ -0,0 +1,7 @@
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+var detail=document.getElementsByClassName("detail");
+console.log(detail);
